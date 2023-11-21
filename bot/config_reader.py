2be0a7f00434aa875b6997fea12d4458f03f75ea
Dmitriy Cherenkov <happyproger01@gmail.com>
2023-11-21 22:26:28 +0300

    feat: split the database path and redis path in config_reader.py

--- bot/config_reader.py
+++ bot/config_reader.py
@@ -5,8 +5,24 @@ from pydantic import SecretStr, PostgresDsn, RedisDsn
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file='.env', env_file_encoding='utf-8')
     bot_token: SecretStr
-    db_url: PostgresDsn
-    redis_url: RedisDsn
+    DB_DRIVER: str
+    DB_USER: str
+    DB_PASS: str
+    DB_HOST: str
+    DB_PORT: int
+    DB_NAME: str
+
+    REDIS_DRIVER: str
+    REDIS_HOST: str
+    REDIS_PORT: int
+
+    @property
+    def db_url(self) -> PostgresDsn:
+        return f'{self.DB_DRIVER}://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}'
+
+    @property
+    def redis_url(self) -> RedisDsn:
+        return f'{self.REDIS_DRIVER}://{self.REDIS_HOST}:{self.REDIS_PORT}'
 
 
 config = Settings()
