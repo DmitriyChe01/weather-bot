60b8bed3cbced1796e304d3d41bf973381bfd0c7
Dmitriy Cherenkov <happyproger01@gmail.com>
2023-12-07 16:34:57 +0300

    Revert "fix: fix production  files"
    This reverts commit 55e222f75adddca739e142ccd4be9233578a2bad.

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,27 +1,16 @@
 import asyncio
 import logging
 
-from aiohttp import web
 from aiogram import Bot, Dispatcher
 from aiogram.utils.callback_answer import CallbackAnswerMiddleware
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 from aiogram.fsm.storage.redis import RedisStorage
-from aiogram.webhook.aiohttp_server import SimpleRequestHandler, setup_application
 
 from config_reader import config
 from handlers import cmd_handlers
 from handlers.FSM import search_city
 from handlers import inline_handlers
 from middlewares.database import DbSessionMiddleware
-
-
-async def on_startup(dp, bot):
-    await bot.set_webhook(config.webhook_url)
-    print(f'Telegram servers now send updates to {config.webhook_url}. Bot is online')
-
-
-async def on_shutdown(dp, bot):
-    await bot.delete_webhook(config.webhook_url)
 
 
 async def main():
@@ -46,18 +35,7 @@ async def main():
     dp.include_router(inline_handlers.router)
 
     await bot.delete_webhook(drop_pending_updates=True)
-
-    if config.webhook:
-        app = web.Application()
-        webhook_requests_handler = SimpleRequestHandler(
-            dispatcher=dp,
-            bot=bot
-        )
-        webhook_requests_handler.register(app, path=config.WEBHOOK_PATH)
-        setup_application(app, dp, bot=bot)
-        web.run_app(app, host=config.WEB_SERVER_HOST, port=config.WEB_SERVER_PORT)
-    else:
-        await dp.start_polling(bot)
+    await dp.start_polling(bot)
 
 
 if __name__ == "__main__":
--- bot/config_reader.py
+++ bot/config_reader.py
@@ -1,5 +1,5 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
-from pydantic import SecretStr, PostgresDsn, RedisDsn, IPvAnyAddress
+from pydantic import SecretStr, PostgresDsn, RedisDsn
 
 
 class Settings(BaseSettings):
@@ -17,13 +17,7 @@ class Settings(BaseSettings):
     REDIS_HOST: str
     REDIS_PORT: int
 
-    WEB_HOOK: bool
-
-    NGINX_HOST: str  # domain
-    WEBHOOK_PATH: str  # path
-
-    WEB_SERVER_HOST: IPvAnyAddress
-    WEB_SERVER_PORT: int
+    NGINX_HOST: str
 
     @property
     def db_url(self) -> PostgresDsn:
@@ -32,14 +26,6 @@ class Settings(BaseSettings):
     @property
     def redis_url(self) -> RedisDsn:
         return f'{self.REDIS_DRIVER}://{self.REDIS_HOST}:{self.REDIS_PORT}'
-
-    @property
-    def webhook_host(self) -> str:
-        return f'https://{self.NGINX_HOST}'
-
-    @property
-    def webhook_url(self) -> str:
-        return f'{self.webhook_host}{self.WEBHOOK_PATH}'
 
 
 config = Settings()
